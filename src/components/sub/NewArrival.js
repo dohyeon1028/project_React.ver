9e40a641181fb4a81efa603b684af012f5482fc1
dohyeon1028 <kdh051028a@gmail.com>
2023-10-10 10:48:20 +0900

    gh-pages 이미지 오류 수정 시도

--- src/components/sub/NewArrival.js
+++ src/components/sub/NewArrival.js
@@ -3,7 +3,7 @@ import FilterWrap from "../common/Filter_wrap";
 import ScrollToTop from "../../function/ScrollToTop";
 
 function NewArrival() {
-    useEffect(()=>{
+    useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
     return (
@@ -17,8 +17,8 @@ function NewArrival() {
                         <section>
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/1.jpg" alt="GOALE POST PREMIUM (고알레 포스트 프리미엄) 풋살화 / SKY BLUE" className="before" />
-                                    <img src="../img/New/1_hover.jpg" alt="GOALE POST PREMIUM (고알레 포스트 프리미엄) 풋살화 / SKY BLUE" className="after" />
+                                    <img src="./img/New/1.jpg" alt="GOALE POST PREMIUM (고알레 포스트 프리미엄) 풋살화 / SKY BLUE" className="before" />
+                                    <img src="./img/New/1_hover.jpg" alt="GOALE POST PREMIUM (고알레 포스트 프리미엄) 풋살화 / SKY BLUE" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">GOALE POST PREMIUM (고알레 포스트 프리미엄) 풋살화 / SKY BLUE</a>
@@ -28,8 +28,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/2.jpg" alt="GOALE POST (고알레 포스트) 풋살화 / MULTI COLOR" className="before" />
-                                    <img src="../img/New/2_hover.jpg" alt="GOALE POST (고알레 포스트) 풋살화 / MULTI COLOR" className="after" />
+                                    <img src="./img/New/2.jpg" alt="GOALE POST (고알레 포스트) 풋살화 / MULTI COLOR" className="before" />
+                                    <img src="./img/New/2_hover.jpg" alt="GOALE POST (고알레 포스트) 풋살화 / MULTI COLOR" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">GOALE POST (고알레 포스트엄) 풋살화 / MULTI COLOR</a>
@@ -39,8 +39,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/3.jpg" alt="고알레 풋볼 넌슬립 크루 삭스 / SKY BLUE" className="before" />
-                                    <img src="../img/New/3_hover.jpg" alt="고알레 풋볼 넌슬립 크루 삭스 / SKY BLUE" className="after" />
+                                    <img src="./img/New/3.jpg" alt="고알레 풋볼 넌슬립 크루 삭스 / SKY BLUE" className="before" />
+                                    <img src="./img/New/3_hover.jpg" alt="고알레 풋볼 넌슬립 크루 삭스 / SKY BLUE" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">고알레 풋볼 넌슬립 크루 삭스 / SKY BLUE</a>
@@ -50,8 +50,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/4.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / OATMEAL BEIGE" className="before" />
-                                    <img src="../img/New/4_hover.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / OATMEAL BEIGE" className="after" />
+                                    <img src="./img/New/4.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / OATMEAL BEIGE" className="before" />
+                                    <img src="./img/New/4_hover.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / OATMEAL BEIGE" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">[얼리버드쿠폰]파이핑 후드 티셔츠 / OATMEAL BEIGE
@@ -62,8 +62,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/5.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / D.RED" className="before" />
-                                    <img src="../img/New/5_hover.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / D.RED" className="after" />
+                                    <img src="./img/New/5.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / D.RED" className="before" />
+                                    <img src="./img/New/5_hover.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / D.RED" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">[얼리버드쿠폰]파이핑 후드 티셔츠 / D.RED</a>
@@ -73,8 +73,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/6.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / BLACK" className="before" />
-                                    <img src="../img/New/6_hover.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / BLACK" className="after" />
+                                    <img src="./img/New/6.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / BLACK" className="before" />
+                                    <img src="./img/New/6_hover.jpg" alt="[얼리버드쿠폰]파이핑 후드 티셔츠 / BLACK" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">[얼리버드쿠폰]파이핑 후드 티셔츠 / BLACK</a>
@@ -84,8 +84,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/7.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="before" />
-                                    <img src="../img/New/7_hover.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="after" />
+                                    <img src="./img/New/7.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="before" />
+                                    <img src="./img/New/7_hover.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE</a>
@@ -95,8 +95,8 @@ function NewArrival() {
 
                             <article>
                                 <div className="imgBox">
-                                    <img src="../img/New/8.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="before" />
-                                    <img src="../img/New/8_hover.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="after" />
+                                    <img src="./img/New/8.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="before" />
+                                    <img src="./img/New/8_hover.jpg" alt="[얼리버드쿠폰]파이핑 맨투맨 / OATMEAL BEIGE" className="after" />
                                 </div>
                                 <figcaption>
                                     <a href="#;">[얼리버드쿠폰]파이핑 맨투맨 / D.RED</a>
